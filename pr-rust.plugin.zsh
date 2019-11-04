e265324c57a0ba1b1b229f1061c71a3e93db3e97
Grigorii Horos <horosgrisa@gmail.com>
2019-11-04 12:04:59 +0200

    Fix

--- pr-rust.plugin.zsh
+++ pr-rust.plugin.zsh
@@ -5,28 +5,26 @@ DEPENDENCES_ZSH+=( zpm-zsh/helpers zpm-zsh/colors )
 RUST_PREFIX=${RUST_PREFIX:-" "}
 RUST_SUFIX=${RUST_SUFIX:-""}
 
-if command -v zpm >/dev/null; then
+if (( $+function[zpm] )); then
   zpm zpm-zsh/helpers zpm-zsh/colors
 fi
 
 pr_rust=""
 
 _pr_rust() {
-  
   if (( $+commands[rustc] )); then
     if is-recursive-exist Cargo.toml >/dev/null ; then
       pr_rust="$RUST_PREFIX"
       
       rust_version=$(rustc --version | cut -d' ' -f2)
       pr_rust+="%{$c[red]${c_bold}%}𝗥%{$c_reset%} %{$c[blue]$c_bold%}$rust_version%{$c_reset%}"
-      
       pr_rust+="$RUST_SUFIX"
+
       return 0
     fi
   fi
   
   pr_rust=""
-  
 }
 
 _pr_rust
