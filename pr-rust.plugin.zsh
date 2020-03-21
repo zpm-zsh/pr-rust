4edea324923cab5e5db8cd4f63ab47be8fa63c7e
Grigorii Horos <horosgrisa@gmail.com>
2020-03-21 13:17:49 +0200

    Optimize plugin

--- pr-rust.plugin.zsh
+++ pr-rust.plugin.zsh
@@ -4,13 +4,13 @@ typeset -g RUST_PREFIX=${RUST_PREFIX:-" "}
 typeset -g RUST_SUFIX=${RUST_SUFIX:-""}
 typeset -g pr_rust=""
 
-if (( $+functions[zpm] )); then
-  zpm zpm-zsh/helpers zpm-zsh/colors
-fi
+if (( $+functions[zpm] )); then #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+  zpm zpm-zsh/helpers zpm-zsh/colors #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+fi #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
 
 if (( $+commands[rustc] )); then
   function _pr_rust() {
-    if is-recursive-exist Cargo.toml >/dev/null ; then
+    if is-recursive-exist Cargo.toml; then
       rust_version=$(rustc --version | cut -d' ' -f2)
 
       pr_rust="${RUST_PREFIX}%{$c[red]${c_bold}%}𝗥%{$c_reset%} %{$c[blue]$c_bold%}${rust_version}%{$c_reset%}${RUST_SUFIX}"
