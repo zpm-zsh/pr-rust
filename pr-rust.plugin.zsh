d54800a7bd0a5ec6d0f7b27abcba705a3b370f81
Grigorii Horos <horosgrisa@gmail.com>
2023-08-18 00:21:09 +0300

    Change ui

--- pr-rust.plugin.zsh
+++ pr-rust.plugin.zsh
@@ -10,13 +10,11 @@ if (( $+commands[rustc] )); then
     pr_rust=''
 
     if is-recursive-exist Cargo.toml; then
-      pr_rust=" %{${c[base3]}${c[bold]}%}(%{${c[reset]}%}"
+      pr_rust=' '
 
       rust_version=$(rustc --version | cut -d' ' -f2)
 
       pr_rust+="%{${c[orange]}${c[bold]}%}${PR_RUST_SYMBOL}%{${c[reset]}%} %{${c[orange]}${c[bold]}%}${rust_version}%{${c[reset]}%}"
-
-      pr_rust+="%{${c[base3]}${c[bold]}%})%{${c[reset]}%}"
     fi
 
   }
