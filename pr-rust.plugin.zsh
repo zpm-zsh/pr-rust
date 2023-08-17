e67277a174e5c7a5cc0dcab2e1a3f0b634765697
Grigorii Horos <horosgrisa@gmail.com>
2023-08-18 00:04:25 +0300

    Change ui

--- pr-rust.plugin.zsh
+++ pr-rust.plugin.zsh
@@ -1,18 +1,24 @@
 #!/usr/bin/env zsh
 
+: ${PR_RUST_SYMBOL:=''}
+
+
 typeset -g pr_rust=''
 
 if (( $+commands[rustc] )); then
   function _pr_rust() {
+    pr_rust=''
+
     if is-recursive-exist Cargo.toml; then
+      pr_rust=" %{${c[base3]}${c[bold]}%}(%{${c[reset]}%}"
+
       rust_version=$(rustc --version | cut -d' ' -f2)
 
-      pr_rust=" %{${c[red]}${c[bold]}%}RUST%{${c[yellow]}%}:%{${c[blue]}%}${rust_version}%{${c[reset]}%}"
+      pr_rust+="%{${c[orange]}${c[bold]}%}${PR_RUST_SYMBOL}%{${c[reset]}%} %{${c[orange]}${c[bold]}%}${rust_version}%{${c[reset]}%}"
 
-      return 0
+      pr_rust+="%{${c[base3]}${c[bold]}%})%{${c[reset]}%}"
     fi
 
-    pr_rust=""
   }
 
   autoload -Uz add-zsh-hook
